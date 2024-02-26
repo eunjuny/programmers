8f48ad0dad47fbdaa66db015e111edf2bc083384
Yeop Eunjun <105467796+eunjuny@users.noreply.github.com>
2024-02-27 00:07:05 +0900

    Update 개인정보 수집 유효기간.java

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 class Solution {
     public List<Integer> solution(String today, String[] terms, String[] privacies) {
-        List<Integer> answer = new ArrayList<>();
+        List<Integer> answer = new ArrayList<>();    // new ArrayList<>()는 동적으로 크기가 조절됨.
         
         LocalDate parsedDate = toDate(today);
         HashMap<String, Integer> map = new HashMap<>();
@@ -15,7 +15,8 @@ class Solution {
         for (int i = 0; i < privacies.length; i++) {
             LocalDate priDate = toDate(privacies[i].split(" ")[0]);
             String priType = privacies[i].split(" ")[1];
-            if (parsedDate.compareTo(priDate.plusMonths(map.get(priType))) >= 0) {
+            // a.compareTo(b)는 a가 더 크면 1, 같으면 0, 작은면 -1을 출력, 같은날이어도 파기해야 하므로 >=
+            if (parsedDate.compareTo(priDate.plusMonths(map.get(priType))) >= 0) {    
                 answer.add(i+1);
             }
         }
@@ -25,6 +26,7 @@ class Solution {
     }
     
     private LocalDate toDate(String input) {
+        // String 문자열을 LocalDate타입으로
         LocalDate date = LocalDate.parse(input, DateTimeFormatter.ofPattern("yyyy.MM.dd"));
         return date;
     }
