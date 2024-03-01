f330a71f51ecd7ca070385aac2db983f10e594d8
Yeop Eunjun <105467796+eunjuny@users.noreply.github.com>
2024-03-01 22:53:15 +0900

    Create 푸드파이트 대회.java

---
+++ 프로그래머스/1/푸드파이트 대회/푸드파이트 대회.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        String halfAnswer = "";
+
+        for (int i = 1; i < food.length; i++) {
+            if (food[i] % 2 != 0) {
+                food[i] = (food[i] - 1) / 2;
+            } else {
+                food[i] = food[i] / 2;
+            }
+
+            for (int j = 0; j < food[i]; j++) {
+                halfAnswer = halfAnswer + "" + i;
+            }
+        }
+        StringBuffer sb = new StringBuffer(halfAnswer);       
+        answer = halfAnswer + "0" + sb.reverse().toString();
+
+        return answer;
+    }
+}
