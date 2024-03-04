dd0352c5c1d11da9a6fad6569f9d19eb9d38fac3
Yeop Eunjun <105467796+eunjuny@users.noreply.github.com>
2024-03-04 23:19:59 +0900

    [level 1] Title: 콜라 문제, Time: 0.02 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        
+        int x = 10000000;
+        
+        while(n / a > 0) {
+            x = (n / a) * b;
+            int y = n % a;
+
+            answer += x;
+            n = x + y;
+        }
+        
+        if (n >= a) {
+            answer = answer + ((n / a) * b);
+        }
+        
+        return answer;
+    }
+}
