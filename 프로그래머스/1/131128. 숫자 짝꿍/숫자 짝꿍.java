1e5c06861f3e0230e65e9289ddbee349eca50efa
Yeop Eunjun <105467796+eunjuny@users.noreply.github.com>
2024-03-07 00:18:07 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.03 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -1,58 +1,46 @@
-// class Solution {
-//     public String solution(String X, String Y) {
-//         StringBuilder answer = new StringBuilder();
-//         char[] sx = X.toCharArray();
-//         char[] sy = Y.toCharArray();
-            
-//         for (int i = 0; i < sx.length; i++) {
-//             for (int j = 0; j < sy.length; j++) {
-//                 if (sx[i] == sy[j]) {
-//                     answer.append(sx[i]);
-//                     // 중복 매치를 피하기 위해 Y에서 일치한 문자를 제거합니다.
-//                     sy[j] = 'a'; // 여러분이 사용하는 문자열에 없는 문자로 대체할 수 있습니다.
-//                     break;
-//                 }
-//             }
-//         }
-//         return answer.toString();
-//     }
-// }
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public class Solution {
-    public static String solution(String X, String Y) {
-        HashMap<Integer, Integer> xmap = new HashMap<>();
-        HashMap<Integer, Integer> ymap = new HashMap<>();
+class Solution {
+    public String solution(String X, String Y) {
+        StringBuilder answer = new StringBuilder();
+        int[] x = new int[10];
+        int[] y = new int[10];
         
-        for(String data : X.split("")){
-            int cur = Integer.parseInt(data);
-            xmap.put(cur, xmap.getOrDefault(cur, 0) + 1);
+        // X와 Y의 각 숫자별 개수를 카운트
+        for(int i = 0; i < X.length(); i++){
+            x[X.charAt(i) - '0']++;
         }
-
-        for(String data : Y.split("")){
-            int cur = Integer.parseInt(data);
-            ymap.put(cur, ymap.getOrDefault(cur, 0) + 1);
+        for(int i = 0; i < Y.length(); i++){
+            y[Y.charAt(i) - '0']++;
         }
+        /*
+        여기서 '0'은 아스키 코드 값이며, '0'의 아스키 코드 값은 48입니다. 그리고 문자열에서 숫자 문자의 아스키 코드 값은 해당 숫자에 48을 빼면 됩니다. 따라서 '1' - '0'은 1이 됩니다.
 
-        StringBuilder sb = new StringBuilder();
+예를 들어, 문자열 "123"이 있다면,
 
-        for(int i = 9; i >= 0 ; i --){
-            if(xmap.containsKey(i) && ymap.containsKey(i)){
-                int cnt = Math.min(xmap.get(i), ymap.get(i));
-                for (int j = 0; j < cnt; j++) {
-                    sb.append(i);
-                }
+'1' - '0'은 1이 되어서 x[1]이 1 증가합니다.
+'2' - '0'은 2가 되어서 x[2]이 1 증가합니다.
+'3' - '0'은 3이 되어서 x[3]이 1 증가합니다.
+즉, 이 코드는 문자열에서 각 숫자의 개수를 카운트하는 역할을 합니다.
+        */
+        // 공통으로 나타나는 숫자들을 이용하여 가장 큰 정수를 만듦
+        for(int i = 9; i >= 0; i--){
+            for(int j = 0; j < Math.min(x[i], y[i]); j++){
+                answer.append(i);
             }
         }
-
-        if(sb.toString().startsWith("0")){
-            return "0";
-        }
-        else if(sb.toString().equals("")){
+        
+        // 결과가 없으면 -1 반환
+        if (answer.length() == 0) {
             return "-1";
+        } 
+        // 결과가 0으로만 이루어져 있으면 0 하나만 반환
+        else if (answer.charAt(0) == '0') {
+            return "0";
+        } 
+        // 결과 반환
+        else {
+            return answer.toString();
         }
-
-        return sb.toString();
     }
 }
+
+
