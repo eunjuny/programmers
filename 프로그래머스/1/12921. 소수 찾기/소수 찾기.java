439ef96a05e899b48d7f638a7484fbaca1e63e5f
Yeop Eunjun <105467796+eunjuny@users.noreply.github.com>
2024-03-30 12:43:06 +0900

    [level 1] Title: 소수 찾기, Time: 196.36 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,27 +1,24 @@
 class Solution {
     public int solution(int n) {
-    	// 2는 소수이기 때문에 초기값 1로 선언
-        int answer = 1;
+    	int answer = 0;
         
-        // 3부터 n까지 반복하여 소수의 개수 찾기
-        for (int i = 3; i <= n; i++) {
-            if (isPrime(i)) answer += 1;
+        for (int i = 2; i <= n; i++) {
+            answer += isPrime(i);
         }
         
         return answer;
     }
     
-    // 소수 판별 메서드
-    private boolean isPrime(int num) {
-    	// 2부터 num의 제곱근까지 반복
-        for (int i = 2; i <= (int) Math.sqrt(num); i++) {
-        	// num이 i로 나눈 나머지가 0이면 소수가 아님
-            if (num % i == 0) {
-                return false;
-            }
-        }
+    private int isPrime(int i) {
+        int result = 1;
         
-        // 반복문을 무사히 마치면 소수
-        return true;
+        for (int j = 2; j <= Math.sqrt(i); j++) {
+            if (i % j == 0) {
+                result = 0;
+                break;
+            }    
+        }
+    
+        return result;
     }
 }
