1c415da187939bc4c03095b152bd694cbf2b0dc7
Yeop Eunjun <105467796+eunjuny@users.noreply.github.com>
2024-03-30 12:29:32 +0900

    Update 실패율.java

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -4,8 +4,12 @@ class Solution {
     public int[] solution(int N, int[] stages) {
         
         HashMap<Integer, Double> map = new HashMap<>();
-        int[] userFailCnts = new int[N+2];
-        int[] userTotalCnts = new int[N+1];
+        // 해당 스테이지에 도전했지만 실패한 인원 수 
+        // userFailCnts[N+1]값을 얻기 위해 크기를 N+2로 설정 (userFialCnts[1]에 1번 스테이지를 표현하기 위해)
+        int[] userFailCnts = new int[N+2];  
+        
+        // 해당 스테이지에 도달한 인원 수
+        int[] userTotalCnts = new int[N+1]; 
         
         for (int stage : stages) {
             userFailCnts[stage]++;
@@ -19,14 +23,14 @@ class Solution {
         
         for (int i = 1; i < userTotalCnts.length; i++) {
             if (userFailCnts[i] == 0 || userTotalCnts[i] == 0) {
-                map.put(i, 0.0);
+                map.put(i, 0.0);    //  해당 스테이지를 도전하고 있는 인원이 없으므로 실패율 0
             } else {
                 map.put(i, (double)userFailCnts[i] / userTotalCnts[i]);
             }
         }
         
         List<Integer> list = new ArrayList<>(map.keySet());
-        Collections.sort(list, (o1, o2) -> Double.compare(map.get(o2), map.get(o1)));
+        Collections.sort(list, (o1, o2) -> Double.compare(map.get(o2), map.get(o1)));   // 내림차순 정렬
         
         return list.stream().mapToInt(Integer::intValue).toArray();
     }
