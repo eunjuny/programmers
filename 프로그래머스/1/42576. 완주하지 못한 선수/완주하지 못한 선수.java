b0bd4c3dc7ee744e3c98e8c657ff9d2d7e7d7581
Yeop Eunjun <105467796+eunjuny@users.noreply.github.com>
2024-04-03 22:53:17 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 65.12 ms, Memory: 97.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> map = new HashMap<>();   
+     
+        // 참가자 목록을 해시맵에 추가하고, 이름이 같은 경우 카운트를 증가시킴
+        for (String name : participant) {
+            map.put(name, map.getOrDefault(name, 0) + 1);
+        }
+        
+        // 완주자 목록을 해시맵에서 제거
+        for (String name : completion) {
+            map.put(name, map.get(name) - 1);
+        }
+        
+        // 카운트가 0이 아닌 이름이 완주하지 못한 참가자
+        for (String name : map.keySet()) {
+            if (map.get(name) != 0) {
+                return name;
+            }
+        }
+        
+        return null;
+    }
+}
