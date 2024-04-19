77b7f4f7f48362f0a5e610590fb7f537b99ec351
Yeop Eunjun <105467796+eunjuny@users.noreply.github.com>
2024-04-20 01:55:11 +0900

    Update 다른 답변.java

--- 프로그래머스/2/12911. 다음 큰 숫자/다른 답변.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다른 답변.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         // 1. 입력받은 n의 1 개수를 저장.
-        int n_cnt = Integer.bitCount(n);
+        int n_cnt = Integer.bitCount(n);    // bitCount(n)는 입력된 n을 2진수로 변환한 뒤, 2진수에 포함된 1을 카운팅
         // 2. 증가하는 n의 1 개수를 저장 할 변수.
         int b_cnt = 0;
         // 3. 무한반복.
