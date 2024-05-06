858ce639e73f63ed1376d704b1f6b00428744f6d
Yeop Eunjun <105467796+eunjuny@users.noreply.github.com>
2024-05-07 01:17:39 +0900

    [level 2] Title: n^2 배열 자르기, Time: 4.87 ms, Memory: 98.8 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -2,15 +2,17 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int n, long left, long right) {
-             int[] answer = new int[(int) (right - left) + 1];
- 
+        int le = (int) left;
+        int ri = (int) right;
+        int[] answer = new int[ri - le + 1];
+        
+        // 이 행렬은 해당 행과 열의 숫자 중 더 큰 숫자를 값으로 가진다.
         int idx = 0;
         for (long i = left; i <= right; i++) {
             long row = i / n + 1;
             long col = i % n + 1;
             answer[idx++] = (int) Math.max(row, col);
         }
-        
         return answer;
     }
 }
