5bba217f5a42a732a34c6869d00a129c67a4be3b
Yeop Eunjun <105467796+eunjuny@users.noreply.github.com>
2024-05-13 00:41:09 +0900

    [level 2] Title: 튜플, Time: 0.54 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/README.md
+++ 프로그래머스/2/64065. 튜플/README.md
@@ -1,10 +1,10 @@
 # [level 2] 튜플 - 64065 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065?language=java) 
 
 ### 성능 요약
 
-메모리: 85.3 MB, 시간: 0.72 ms
+메모리: 77 MB, 시간: 0.54 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 12일 23:49:20
+2024년 05월 13일 00:41:07
 
 ### 문제 설명
 
--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,33 +1,26 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(String s) {
-        int[] answer = {};
-        s = s.replaceAll("[\\{,\\}]", " ");
-        String[] sa = s.split("[ ]+");
-        Map<String, Integer> hashMap = new HashMap<>();
-
-
-        for(int j = 0; j < sa.length; j++){
-            if (!sa[j].equals("")){
-                if (hashMap.containsKey(sa[j])){
-                    hashMap.replace(sa[j], hashMap.get(sa[j]) + 1);
-                }else{
-                    hashMap.put(sa[j], 1);
-                }
+    public ArrayList<Integer> solution(String s) {
+    
+        ArrayList<Integer> answer = new ArrayList<>();
+        String[] str = s.substring(0,s.length()-2).replace("{","").split("},");
+      
+        Arrays.sort(str, new Comparator<String>(){
+            public int compare(String o1, String o2){
+                return Integer.compare(o1.length(), o2.length());
+            }
+        });
+        
+        for (String st : str) {
+            String[] temp = st.split(",");
+            for (int i = 0; i < temp.length; i++) {
+                int n = Integer.parseInt(temp[i]);
+                if(!answer.contains(n))
+                    answer.add(n);
             }
         }
-
-        List<Map.Entry<String, Integer>> list = new ArrayList<>(hashMap.entrySet());
-        Collections.sort(list, (o1, o2) -> {return o2.getValue().compareTo(o1.getValue());});
-
-        answer = new int[list.size()];
-
-        for(int i = 0; i < answer.length; i++){
-            answer[i] = Integer.parseInt(list.get(i).getKey());
-        }
-
-
-        return answer;
+        
+        return answer; 
     }
 }
