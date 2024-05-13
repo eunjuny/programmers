aa0f79c51434f358ed6d4460e6d0053d458f114d
Yeop Eunjun <105467796+eunjuny@users.noreply.github.com>
2024-05-13 23:15:19 +0900

    [level 2] Title: 프로세스, Time: 3.21 ms, Memory: 69.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,29 @@
+import java.util.PriorityQueue;
+import java.util.Collections;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int priority : priorities) {
+            pq.offer(priority);
+        }
+        
+        while (!pq.isEmpty()) {
+            for (int i = 0; i < priorities.length; i++) {
+                System.out.println(pq);
+                if (pq.peek() == priorities[i]) {
+                    pq.poll();
+                    answer++;
+                    
+                    if (location == i) {
+                        return answer;
+                    }
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
