5a420b70225a6ecbc10be6b5b3c9f69307c9aa99
Yeop Eunjun <105467796+eunjuny@users.noreply.github.com>
2024-05-21 22:52:20 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.08 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,29 +1,35 @@
 class Solution {
     public int solution(int n, int k) {
-        String kNumber = Long.toString(n, k);
         
-        String[] pArr = kNumber.split("0");
+        // n을 k진수로 변환
+        String kNumber = Integer.toString(n, k); 
         
-        int pCount = 0;
+        // k진수로 변환된 문자열을 "0"을 기준으로 분할
+        String[] arr = kNumber.split("0");
         
-        for (String p : pArr) {
-            if (p.isEmpty() || p.isBlank()) continue;
+        int cnt = 0;
+        
+        for (String ar : arr) {
+            // 빈 문자열은 무시
+            if (ar.isEmpty()) continue;
             
-            if (isPrime(Long.parseLong(p))) pCount++;   
+            // 정수로 변환한 후 소수 체크
+            if (isPrime(Long.parseLong(ar))) {
+                cnt++;
+            }
         }
-        return pCount;
+        
+        return cnt;
     }
     
-    public boolean isPrime(long n) {
-        if (n == 1) return false;
-        if (n == 2) return true;
+    private boolean isPrime(long n) {
+        if (n <= 1) return false;
+        if (n == 2 || n == 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
         
-        int count = 0;
-        
-        for (long i = 2; i <= Math.sqrt(n); i++) {
+        for (long i = 5; i*i <= n; i++) {
             if (n % i == 0) return false;
         }
-        
         return true;
     }
 }
