c3203bc426d8a7bef038db0f3a26c60ccca1124e
Yeop Eunjun <105467796+eunjuny@users.noreply.github.com>
2024-05-30 21:33:43 +0900

    Update 방문 길이.java

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,60 +1,62 @@
 import java.util.HashSet;
+
 class Solution {
     public int solution(String dirs) {
         int answer = 0;
         
-        HashSet<String> set = new HashSet<String>();
+        HashSet<String> set = new HashSet<>();
         
         int[][] pos = new int[2][2]; // 현재 위치와 이전 위치 저장 
         
         for (int i = 0; i < dirs.length(); i++) {
-			char direction = dirs.charAt(i);
-			// 이전 위치 저장
-			pos[1][0] = pos[0][0];
-			pos[1][1] = pos[0][1];
-			
-			boolean flag = false;
-			switch (direction) {
-			case 'U':
-				pos[0][1]++;
-				if (pos[0][1] > 5) {
-					flag = true;
-					pos[0][1] = 5;
-				}
-				break;
-			case 'D':
-				pos[0][1]--;
-				if (pos[0][1] < -5) {
-					flag = true;
-					pos[0][1] = -5;
-				}
-				break;
-			case 'R':
-				pos[0][0]++;
-				if (pos[0][0] > 5) {
-					flag = true;
-					pos[0][0] = 5;
-				}
-				break;
-			case 'L':
-				pos[0][0]--;
-				if (pos[0][0] < -5) {
-					flag = true;
-					pos[0][0] = -5;
-				}
-				break;
-			default:
-				break;
-			}
+            char direction = dirs.charAt(i);
+            // 이전 위치 저장
+            pos[1][0] = pos[0][0];
+            pos[1][1] = pos[0][1];
+            
+            boolean flag = false;
+            switch (direction) {
+                case 'U':
+                    pos[0][1]++;
+                    if (pos[0][1] > 5) {
+                        flag = true;
+                        pos[0][1] = 5;
+                    }
+                    break;
+                case 'D':
+                    pos[0][1]--;
+                    if (pos[0][1] < -5) {
+                        flag = true;
+                        pos[0][1] = -5;
+                    }
+                    break;
+                case 'R':
+                    pos[0][0]++;
+                    if (pos[0][0] > 5) {
+                        flag = true;
+                        pos[0][0] = 5;
+                    }
+                    break;
+                case 'L':
+                    pos[0][0]--;
+                    if (pos[0][0] < -5) {
+                        flag = true;
+                        pos[0][0] = -5;
+                    }
+                    break;
+                default:
+                    break;
+            }
 
-			if (flag)
-				continue;
+            if (flag)
+                continue;
 
-			set.add("" + pos[0][0] + pos[0][1] + pos[1][0] + pos[1][1]);
-			set.add("" + pos[1][0] + pos[1][1] + pos[0][0] + pos[0][1]);
-		}
+            // 경로를 양방향으로 저장
+            set.add("" + pos[0][0] + pos[0][1] + pos[1][0] + pos[1][1]);
+            set.add("" + pos[1][0] + pos[1][1] + pos[0][0] + pos[0][1]);
+        }
         
-        answer = set.size() / 2;
+        answer = set.size() / 2; // 양방향 저장된 경로 수를 2로 나누어 반환
         
         return answer;
     }
