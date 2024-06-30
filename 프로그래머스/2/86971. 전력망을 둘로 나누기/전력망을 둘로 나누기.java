00245baebb1f9a4168d82a03ced6dc69721e54b5
Yeop Eunjun <105467796+eunjuny@users.noreply.github.com>
2024-06-30 23:03:32 +0900

    Update 전력망을 둘로 나누기.java

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -20,40 +20,44 @@ class Solution {
             graph[v1].add(v2);
             graph[v2].add(v1);
         }
-     
+        
         for (int i = 0; i < wires.length; i++) {
             int v1 = wires[i][0];
             int v2 = wires[i][1];
- 
-            boolean[] visited = new boolean[n + 1];
- 
-            // 해당 간선을 그래프에서 제거
+            
+            boolean[] visited = new boolean[n+1];
+            
+            // 해당 간선을 그래프에서 제거 후 개수 비교
             graph[v1].remove(Integer.valueOf(v2));
             graph[v2].remove(Integer.valueOf(v1));
- 
-            int cnt = dfs(1, visited); // 임의의 시작점에서 dfs 탐색
- 
+            
+            int cnt = dfs(1, visited);
             int diff = Math.abs(cnt - (n - cnt));
             min = Math.min(min, diff);
- 
+            
             // 그래프에 다시 간선 추가
             graph[v1].add(v2);
             graph[v2].add(v1);
         }
- 
+            
         return min;
     }
- 
+    
     static int dfs(int v, boolean[] visited) {
         visited[v] = true;
         int cnt = 1;
- 
+        
+        /*
+            graph[1] = [2]
+            graph[2] = [1, 3]
+            graph[3] = [2, 4]
+            graph[4] = [3]
+        */
         for (int next : graph[v]) {
             if (!visited[next]) {
                 cnt += dfs(next, visited);
             }
         }
- 
         return cnt;
     }
 }
