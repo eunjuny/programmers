52b95c6784decdeb1e9a98921d3befbc5c60764a
Yeop Eunjun <105467796+eunjuny@users.noreply.github.com>
2024-07-07 23:46:15 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 1.16 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] arrayA, int[] arrayB) {
+        
+        int gcdA = gcd(arrayA);
+        int gcdB = gcd(arrayB);
+        
+        int ans = 0;
+        if (check(arrayA, gcdB)) ans = Math.max(ans, gcdB);
+        if (check(arrayB, gcdA)) ans = Math.max(ans, gcdA);
+        return ans;
+    }
+    
+    private boolean check(int[] arr, int gcd) {
+        for (int i:arr) {
+            if (i%gcd==0) return false;
+        }
+        return true;
+    }
+    
+    private int gcd(int[] arr) {
+        return Arrays.stream(arr).reduce(arr[0], (a, b)->gcd(a, b));
+    }
+    
+    private int gcd(int a, int b) {
+        if (a%b == 0) return b;
+        return gcd(b, a%b);
+    }
+}
