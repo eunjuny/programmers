c29070cd5817397f503aedf6b069bb02021ad6a2
Yeop Eunjun <105467796+eunjuny@users.noreply.github.com>
2024-07-23 01:16:20 +0900

    Update ［카카오 인턴］ 수식 최대화.java

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -1,88 +1,87 @@
 import java.util.*;
 
 class Solution {
-    static long answer = 0;
-    static String op[] = {"+","-","*"};
-    static boolean[] visited = new boolean[3];
-    static ArrayList<Long> numList = new ArrayList<>();
-    static ArrayList<String> opList = new ArrayList<>();
-    static String[] perm = new String[3];
+    static long answer = 0; // 최대값을 저장할 변수
+    static String[] op = {"+","-","*"}; // 사용할 연산자들
+    static boolean[] visited = new boolean[3]; // 연산자가 사용되었는지 확인하기 위한 배열
+    static ArrayList<Long> numList = new ArrayList<>(); // 숫자들을 저장할 리스트
+    static ArrayList<String> opList = new ArrayList<>(); // 연산자들을 저장할 리스트
+    static String[] perm = new String[3]; // 연산자 우선순위 순열을 저장할 배열
     
     public long solution(String expression) {
         String num = "";
         
-        //op, num 구분
-        for(int i=0; i<expression.length(); i++){
+        // 숫자와 연산자를 구분하여 각각 리스트에 저장
+        for (int i = 0; i < expression.length(); i++) {
             char c = expression.charAt(i);
-            if(c=='*' || c=='+' || c=='-'){
-                opList.add(c+""); 
+            if (c == '*' || c == '+' || c == '-') {
+                opList.add(c + ""); 
                 numList.add(Long.parseLong(num));
                 num = "";
-            }
-            else{
+            } else {
                 num += c;
             }
         }
-        //마지막 숫자
+        // 마지막 숫자를 리스트에 추가
         numList.add(Long.parseLong(num));
         
-        //순열 만들기
+        // 연산자의 모든 순열을 생성
         makePermutation(0);
         
         return answer;
     }
     
-    static void makePermutation(int depth){
-        if(depth==op.length){
-            //3개를 선택함 -> 연산
+    static void makePermutation(int depth) {
+        if (depth == op.length) {
+            // 3개의 연산자 순열을 모두 선택한 경우 -> 연산 수행
             sol();
             return;
         }
         
-        for(int i=0; i<op.length; i++){
-            if(visited[i]) continue;
-            visited[i] = true;
-            perm[depth] = op[i];
-            makePermutation(depth+1);
-            visited[i] = false;
+        for (int i = 0; i < op.length; i++) {
+            if (visited[i]) continue; // 이미 선택된 연산자는 건너뜀
+            visited[i] = true; // 연산자를 선택함
+            perm[depth] = op[i]; // 현재 깊이에 연산자 저장
+            makePermutation(depth + 1); // 다음 깊이로 이동
+            visited[i] = false; // 연산자 선택 해제
         }
     }
     
-    static void sol(){
-        //list 복사
-        ArrayList<String> oper = new ArrayList<String>();
-        oper.addAll(opList);
+    static void sol() {
+        // 연산자 리스트 복사
+        ArrayList<String> oper = new ArrayList<>(opList);
         
-        ArrayList<Long> num = new ArrayList<Long>();
-        num.addAll(numList);
+        // 숫자 리스트 복사
+        ArrayList<Long> num = new ArrayList<>(numList);
         
-        //연산자 우선순위에 따라 계산
-        for(int i=0; i<perm.length; i++){
+        // 연산자 우선순위에 따라 계산
+        for (int i = 0; i < perm.length; i++) {
             String op = perm[i];
-            for(int j=0; j<oper.size(); j++){
-                if(oper.get(j).equals(op)){
+            for (int j = 0; j < oper.size(); j++) {
+                if (oper.get(j).equals(op)) {
                     long n1 = num.get(j);
-                    long n2 = num.get(j+1);
-                    long res = cal(n1, n2, op);
+                    long n2 = num.get(j + 1);
+                    long res = cal(n1, n2, op); // 두 숫자를 연산
                     
-                    //list 갱신
-                    num.remove(j+1);
+                    // 리스트 갱신: 사용된 숫자와 연산자를 제거하고 결과를 추가
+                    num.remove(j + 1);
                     num.remove(j);
                     oper.remove(j);
                     
                     num.add(j, res);
                     
-                    j--;
+                    j--; // 다음 연산을 위해 인덱스 조정
                 }
             }
         }
         
+        // 결과의 절대값과 현재 최대값을 비교하여 더 큰 값 저장
         answer = Math.max(answer, Math.abs(num.get(0)));
     }
     
-    static long cal(long n1, long n2, String op){
+    static long cal(long n1, long n2, String op) {
         long res = 0;
-        switch(op){
+        switch (op) {
             case "*": 
                 res = n1 * n2;
                 break;
@@ -93,7 +92,6 @@ class Solution {
                 res = n1 - n2;
                 break;
         }
-        
         return res;
     }
 }
