336ec4aa0a91650b3c43df6363745514c38e99c1
Yeop Eunjun <105467796+eunjuny@users.noreply.github.com>
2025-11-21 01:07:31 +0900

    [level 3] Title: 정수 삼각형, Time: 12.52 ms, Memory: 65.5 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -4,8 +4,11 @@ class Solution {
     public int solution(int[][] triangle) {
         int answer = 0;
         
-        for (int i = triangle.length-2; i >= 0; i--) {
+        // DP(Dynamic Programming)
+        // 각 칸에서 만들 수 있는 경로 합을 아래에서 위로 올라가면서 계산
+        for (int i = triangle.length-2; i >= 0; i--) {  // 맨 아래 줄 바로 위 줄 부터 시작(맨 아래 줄은 이미 최대 합인 상태이므로)
             for(int j =0; j<triangle[i].length; j++) {
+                // 해당 칸에서 가능한 최대 합으로 바꿔가면서 올라감
                 triangle[i][j] += Math.max(triangle[i+1][j], triangle[i+1][j+1]);  
             }
         }
