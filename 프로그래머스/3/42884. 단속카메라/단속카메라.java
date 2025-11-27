e5e037173593b7b25477c1b7cc60883cb16f0850
Yeop Eunjun <105467796+eunjuny@users.noreply.github.com>
2025-11-28 02:04:22 +0900

    [level 3] Title: 단속카메라, Time: 8.47 ms, Memory: 55.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] routes) {
+        
+        Arrays.sort(routes, (a,b) -> a[1] - b[1]);
+        
+        int cam = routes[0][1];
+        int answer = 1;
+        
+        for(int i = 1; i < routes.length; i++) {
+            if(cam >= routes[i][0] && cam <= routes[i][1]) {
+                continue;
+            }
+            cam = routes[i][1];
+            answer++;
+        }
+        
+        return answer;
+    }
+}
