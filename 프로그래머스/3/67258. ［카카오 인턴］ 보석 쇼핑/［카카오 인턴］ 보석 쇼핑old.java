d563ee4a87b2b9ce7f93c0a494f4e172f8cf4f0f
Yeop Eunjun <105467796+eunjuny@users.noreply.github.com>
2025-11-30 13:42:19 +0900

    Update ［카카오 인턴］ 보석 쇼핑old.java

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑old.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑old.java
@@ -11,24 +11,25 @@ class Solution {
         int left = 0;
         int minLen = Integer.MAX_VALUE;
         
+        // 윈도우를 이동하면서 최소 구간 탐색
         for(int i = 0; i < gems.length; i++) {
-            result.put(gems[i], result.getOrDefault(gems[i], 0)+1);
+            result.put(gems[i], result.getOrDefault(gems[i], 0) +1);
             
-            while(result.size() == totalGem) {
-                if(i - left < minLen) {
+            while(result.size() == totalGem) {  // 모든 보석이 다 들어 있다면
+                if(i - left < minLen) { // 왼쪽에서 부터 중복 제거해서 길이 줄이기 위한 작업
                     minLen = i - left;
-                    answer[0] = left +1;
-                    answer[1] = i +1;
+                    answer[0] = left + 1;
+                    answer[1] = i + 1;
                 }
                 
-                result.put(gems[left], result.get(gems[left]) -1);
+                result.put(gems[left], result.get(gems[left])-1); 
                 if(result.get(gems[left]) == 0) {
-                    result.remove(gems[left]);
+                    result.remove(gems[left]);  // 보석이 하나도 안남고 지워졌으면 해당 보석 제거해서 루프 탈출
                 }
-                left++;
-            }
-            
+                left++; // 한칸 증가한 시작점에서 재시작
+            } 
         }
+        
         
         return answer;
     }
