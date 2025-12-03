0b93ba9a05b217708b4f69168e43680bd5a0132d
Yeop Eunjun <105467796+eunjuny@users.noreply.github.com>
2025-12-04 03:43:09 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.47 ms, Memory: 95.1 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,30 +2,29 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] jobs) {
-        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+        Arrays.sort(jobs, (a,b) -> a[0] - b[0]);
         
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        
-        int totalTime = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);    
+    
         int t = 0;
+        int totalTime = 0;
         int jobNum = 0;
         
-        while (jobNum < jobs.length || !pq.isEmpty()) {
-             while(jobNum < jobs.length && jobs[jobNum][0] <= t) {
-                 pq.add(jobs[jobNum++]);
-             }   
+        while(jobNum < jobs.length || !pq.isEmpty()) {
+            while(jobNum < jobs.length && jobs[jobNum][0] <= t) {
+                pq.add(jobs[jobNum++]);
+            }
             
             if(pq.isEmpty()) {
-                t = jobs[jobNum][0];
+                t=jobs[jobNum][0];
                 continue;
-                
             } else {
                 int[] job = pq.poll();
                 t += job[1];
                 totalTime += (t - job[0]);
             }
         }
-        return totalTime/jobs.length;
-        }
         
+        return totalTime / jobs.length;
+    }
 }
