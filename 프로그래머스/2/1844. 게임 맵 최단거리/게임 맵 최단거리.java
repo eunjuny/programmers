75c355246ae5fc1cab5fca6f8d4df02adf85b311
Yeop Eunjun <105467796+eunjuny@users.noreply.github.com>
2025-12-04 02:42:44 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.92 ms, Memory: 58.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,45 +1,39 @@
 import java.util.*;
 
-class Solution {
-    
-    // 상하좌우
-    int[] dx = {0, 0, -1, 1};
-    int[] dy = {1, -1, 0, 0};
+class Solution {        
+    int[] mX = new int[]{0,0,-1,1};
+    int[] mY = new int[]{1,-1,0,0};
     
     public int solution(int[][] maps) {
         int answer = 0;
         
-        int[][] visited = new int[maps.length][maps[0].length];
+        int[][] distance = new int[maps.length][maps[0].length];    // 빈 칸에 0이 자동 초기화됨
+       
+        bfs(maps, distance);
         
-        bfs(maps, visited);
-        
-        answer = visited[maps.length -1][maps[0].length -1];
-        return  answer >= 1 ? answer : -1;
+        answer = distance[maps.length-1][maps[0].length-1];
+        return answer == 0 ? -1 : answer;
     }
     
-    private void bfs(int[][] maps, int[][] visited) {
+    private void bfs(int[][] maps, int[][]distance) {
         Deque<int[]> dq = new ArrayDeque<>();
-        dq.offerFirst(new int[]{0,0}); // 시작 점
-        visited[0][0] = 1; // 시작 점 방문 체크
+        dq.offerLast(new int[]{0,0});
+        distance[0][0] = 1;
         
         while(!dq.isEmpty()) {
             int[] curr = dq.pollFirst();
-            int x = curr[0];
-            int y = curr[1];
+            int x = curr[1];
+            int y = curr[0];
             
-    
-            // 상하좌우 인접 정점 체크
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                
-                if(nx < 0 || ny < 0 || nx >= maps.length || ny >= maps[0].length) {
+            for(int i = 0; i < 4; i++){
+                int toX = x + mX[i];
+                int toY = y + mY[i];
+                if(toX < 0 || toY < 0 || toY >= maps.length || toX >= maps[0].length) {
                     continue;
                 }
-                
-                if(maps[nx][ny] == 1 && visited[nx][ny] ==0) {
-                    visited[nx][ny] = visited[x][y] +1;
-                    dq.offerLast(new int[]{nx, ny});
+                if(maps[toY][toX] != 0 && distance[toY][toX] == 0) {
+                    distance[toY][toX] = distance[y][x] + 1;
+                    dq.offerLast(new int[]{toY, toX});
                 }
             }
         }
