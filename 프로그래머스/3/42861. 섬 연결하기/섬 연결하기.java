fe00c8a018d8670eb6b529ed9f255aecfd68bf9a
Yeop Eunjun <105467796+eunjuny@users.noreply.github.com>
2025-12-04 12:59:24 +0900

    Update 섬 연결하기.java

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,47 +1,52 @@
 import java.util.*;
 
 class Solution {
+    
     int[] parent;
+    
     public int solution(int n, int[][] costs) {
-        
-        // 비용 순으로 오름차순 정렬
-        Arrays.sort(costs, (a, b) -> a[2] - b[2]);
+        //비용 기준 정렬
+        Arrays.sort(costs, (a,b) -> a[2] - b[2]);
         
         parent = new int[n];
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             parent[i] = i;
         }
         
         int total = 0;
         int cnt = 0;
         
-        for (int[] c : costs) {
-            int a = c[0];
-            int b = c[1];
-            int cost = c[2];
+        // 비용 낮은 간선부터 선택
+        for (int[] edge : costs) {
+            int a = edge[0];
+            int b = edge[1];
+            int cost = edge[2];
             
-            if(find(a) != find(b)) {
+            // 서로 다른 부모면 합침
+            if (find(a) != find(b)) {
                 union(a, b);
                 total += cost;
                 cnt++;
+                
+                // 간선 개수는 최대 n-1이라서 n-1이면 MST 완성
+                if(cnt == n -1) break;
             }
-            
-            // 간선 개수는 최대 n-1개
-            if(cnt == n-1) break;
         }
         
         return total;
     }
     
-    private int find(int i) {
-        if(parent[i] == i) return i;
-        
-        return parent[i] = find(parent[i]);
+    // 부모 찾기 - 경로 압축
+    // 처음에는 자기 자신이 부모였지만 나중엔 합쳐져서 부모 하나에 여럿이 속하게 됨
+    private int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
     }
     
+    // 두 집합 합치기
     private void union(int a, int b) {
-        a = find(a);
-        b = find(b);
-        if(a != b) parent[b] = a;
+        int pa = find(a);
+        int pb = find(b);
+        parent[pb] = pa; 
     }
 }
