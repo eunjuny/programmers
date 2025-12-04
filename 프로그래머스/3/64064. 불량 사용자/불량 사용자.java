9ba6c7ae2df9731b99a9d7a85e5298de7ef99ee6
Yeop Eunjun <105467796+eunjuny@users.noreply.github.com>
2025-12-04 14:29:53 +0900

    Update 불량 사용자.java

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -25,11 +25,15 @@ class Solution {
 
     // user_id 배열의 크기가 1이상 8이하 이기 때문에 mask 사용 용이
     private void dfs(int idx, int mask) {
+        // 중복이 모두 제거된 채 모든 ban을 체크했다면 해당 mask값 추가 (mask는 ban되는 유저들의 index를 2진수로 표현한 값)
         if (idx == matches.size()) {
             answers.add(mask);
             return;
         }
 
+        /*
+        matches에서 첫번째 ban에 매칭되는 목록을 호출해서 하나를 선택하고 다음 ban에 해당되는 목록을 호출한다. 이때 앞에서 사용한 user가 중복 사용되는지 체크한다.
+        */
         for (int userIndex : matches.get(idx)) {
             // 이미 선택된 유저면 건너뜀, 1 << userIndex는 1을 2진수 비트에서 userIndex만큼 이동시킨다.
             /*
