ec131908888a1f1a31a2acfa14eec72a5864f459
Yeop Eunjun <105467796+eunjuny@users.noreply.github.com>
2025-12-04 13:27:36 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 0.02 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.java
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.java
@@ -1,13 +1,12 @@
 class Solution {
     public int solution(int n) {
-        
         int[] dp = new int[n+1];
         
         dp[0] = 1;  // 카탈란 수 기본값  (0쌍은 1가지)
         dp[1] = 1;  // 1쌍은 1가지
         
         for(int i = 2; i <= n; i++) {
-            for(int j = 0; j < i; j++){
+            for(int j = 0; j < i; j++) {
                 dp[i] += dp[j] * dp[i - 1 - j];
             }
         }
@@ -15,3 +14,9 @@ class Solution {
         return dp[n];
     }
 }
+
+/*
+dp[3] = dp[0]*dp[2] + dp[1]*dp[1] + dp[2]*dp[0]
+      = 1*2 + 1*1 + 2*1
+      = 5
+*/
